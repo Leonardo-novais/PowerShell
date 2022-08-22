f985cb5a70e95414fc3206dfef42d78c0203a09f
Leonardo Ferreira <73661429+Leonardo-novais@users.noreply.github.com>
2022-08-21 22:15:20 -0300

    Create ping-sweep.ps1

---
+++ ping-sweep.ps1
@@ -0,0 +1,13 @@
+#DESEC SECURITY - PING SWHEEP
+param($p1)
+if (!$p1){
+ echo "Desec Security"
+ echo "Exemplo de uso: ./script.ps1 192.168.0"
+
+} else {
+foreach ($ip in 1..254){
+try {$resp = ping -n 1 "$p1.$ip" | Select-String "bytes=32"
+$resp.Line.Split('')[2] -replace ":",""
+} catch {}
+}
+}
